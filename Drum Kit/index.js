f3fd184efcf042cd6e19155d7343a5f8079a3507
Mynor Morales <mynormh@outlook.com>
2019-01-21 21:57:34 -0600

    Stop if there's no sound and rewind if played repeatedly

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,4 +1,6 @@
 window.addEventListener("keydown", function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
   audio.play();
 });
