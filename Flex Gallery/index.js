f32dd1cbbfe81cbef57f9c07009dd0dbc63504f1
Mynor Morales <mynormh@outlook.com>
2019-01-28 20:47:11 -0600

    Closes previous open panel before opening a new one

--- Flex Gallery/index.js
+++ Flex Gallery/index.js
@@ -1,7 +1,15 @@
 const panels = document.querySelectorAll(".panel");
 
 function toggleOpen() {
+  closePanels();
   this.classList.toggle("open");
+}
+
+function closePanels() {
+  const openPanel = document.querySelector(".panel.open");
+  if (openPanel) {
+    openPanel.classList.remove("open");
+  }
 }
 
 function toggleActive(e) {
