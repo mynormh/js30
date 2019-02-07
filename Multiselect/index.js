29ab24ff46004c8345e7738997c07124d39df5cd
Mynor Morales <mynormh@outlook.com>
2019-02-06 21:23:39 -0600

    Adds functionality to multiselect

---
+++ Multiselect/index.js
@@ -0,0 +1,23 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let firstChecked;
+
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === firstChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+      console.log(checkbox);
+    });
+  }
+  firstChecked = this;
+  console.log(firstChecked);
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener("click", handleCheck));
