9e175eb81379ba982fa723c1c6f12ce224ea8ca3
Mynor Morales <mynormh@outlook.com>
2019-02-11 21:11:57 -0600

    Checks specifically for the konami code

--- Konami Code/index.js
+++ Konami Code/index.js
@@ -1,10 +1,12 @@
+const info = document.querySelector("p");
 const pressed = [];
-const secretCode = "mynormh";
+const secretCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 window.addEventListener("keyup", e => {
-  pressed.push(e.key);
+  pressed.push(e.keyCode);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-  if (pressed.join("").includes(secretCode)) {
+  if (pressed.join("").includes(secretCode.join(""))) {
+    info.innerText = "";
     console.log("DING DING");
     cornify_add();
   }
