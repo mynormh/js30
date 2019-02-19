cbd2f1893d2570077847e59b5d1eba72bb19f9e6
Mynor Morales <mynormh@outlook.com>
2019-02-18 19:42:44 -0600

    Adds more shadows

--- Mouse Shadow/index.js
+++ Mouse Shadow/index.js
@@ -13,7 +13,11 @@ function shadow(e) {
 
   const xWalk = Math.round((x / width) * walk - walk / 2);
   const yWalk = Math.round((y / height) * walk - walk / 2);
-  text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7)`;
+  text.style.textShadow = `
+  ${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7),
+  ${xWalk * -1}px ${yWalk}px 0 rgba(0, 255, 255, 0.7),
+  ${xWalk}px ${yWalk * -1}px 0 rgba(0, 255, 255, 0.7),
+  ${xWalk * -1}px ${yWalk}px 0 rgba(0, 0, 255, 0.7)`;
 }
 
 hero.addEventListener("mousemove", shadow);
