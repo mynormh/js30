ba9a766f8f8c08c6019d6cdad30061b8e45761e5
Mynor Morales <mynormh@outlook.com>
2019-03-04 20:51:00 -0600

    Adds sticky nav

---
+++ Sticky Nav/index.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector("#main");
+const navTop = nav.offsetTop;
+
+function fixNav() {
+  console.log(navTop, window.scrollY);
+  if (window.scrollY >= navTop) {
+    document.body.style.paddingTop = `${nav.offsetHeight}px`;
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
